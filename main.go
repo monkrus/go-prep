bb5bd5c091efe9f03304e34e4d7532fe5655ce35
Sergei Stadnik <sergeisqa@gmail.com>
2020-06-21 21:25:39 -0700

    upd

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"net/url"
 
 	"github.com/gorilla/mux"
 )
@@ -29,27 +28,13 @@ func main() {
 	router := mux.NewRouter()
 	//then you used it for described below line
 	router.HandleFunc("/hello", helloLink).Methods("GET")
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./sergei.html")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "sergei.html")
 	})
-	router.HandleFunc("/postform", func(w http.ResponseWriter, r *http.Request) {
-		//CHOOSE ONE (BUT FIRST MORE AGILE)
-		//FIRST VARIANT
-		query, _ := url.ParseQuery(r.URL.RawQuery)
-		if value, ok := query["name"]; ok {
-			fmt.Fprintf(w, "Name: %s", value[0])
-		} else {
-			fmt.Fprintf(w, "Oh, i don't know your name, stranger")
-		}
-		//SECOND VARIANT
-		query2 := r.URL.Query().Get("name")
-		if query2 != "" {
-			fmt.Fprintf(w, "Name: %s", query2)
-		} else {
-			fmt.Fprintf(w, "Oh, i don't know your name, stranger")
-		}
+	http.HandleFunc("/postform", func(w http.ResponseWriter, r *http.Request) {
+		name := r.FormValue("username")
+		fmt.Fprintf(w, "Name: %s", name)
 	})
-	//as second parametr you set "router" - it meens that ListenAndServe method accepted only "router" method
 	http.ListenAndServe(":8081", router)
 
 }
