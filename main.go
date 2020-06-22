25fe9dd6f2efcac19d8e9e6d60a3ef91cc4ae390
naiks10 <ultravirus661@gmail.com>
2020-06-22 12:45:13 +0300

    added review #1

--- main.go
+++ main.go
@@ -24,15 +24,20 @@ func helloLink(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	//here you initialized Gorilla's router
 	router := mux.NewRouter()
+	//then you used it for described below line
 	router.HandleFunc("/hello", helloLink).Methods("GET")
+	//after this, you used static method from net/http package (So, where is router?)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "sergei.html")
+		http.ServeFile(w, r, "./sergei.html")
 	})
+	//and again http
 	http.HandleFunc("/postform", func(w http.ResponseWriter, r *http.Request) {
 		name := r.FormValue("username")
 		fmt.Fprintf(w, "Name: %s", name)
 	})
+	//such as second parametr you set "router" - it meens that ListenAndServe method accepted only "router" method
 	http.ListenAndServe(":8081", router)
 
 }
