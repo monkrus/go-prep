c7de8822aba0df67c934860aff4ce0909288911a
Sergei Stadnik <sergeisqa@gmail.com>
2020-06-23 09:08:01 -0700

    Attempt to check port

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/monkrus/go-prep.git
 
 go 1.14
 
-require github.com/gorilla/mux v1.7.4
+require (
+	github.com/anvie/port-scanner v0.0.0-20180225151059-8159197d3770
+	github.com/gorilla/mux v1.7.4
+)
--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"net/url"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -26,6 +28,24 @@ func helloLink(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	{ //getting a 'too many colons' error
+		hostName := "localhost:8081"
+		portNum := "8081"
+		seconds := 5
+		timeOut := time.Duration(seconds) * time.Second
+
+		conn, err := net.DialTimeout("tcp", hostName+":"+portNum, timeOut)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Printf("Connection established between %s and localhost with time out of %d seconds.\n", hostName, int64(seconds))
+		fmt.Printf("Remote Address : %s \n", conn.RemoteAddr().String())
+		fmt.Printf("Local Address : %s \n", conn.LocalAddr().String())
+
+	}
 	//initialize Gorilla/Mux router
 	router := mux.NewRouter()
 	// utilize router below
